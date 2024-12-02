3501812b0e387e3bc51172647078a27320d35bee
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-02 20:03:47 +0100

    Created Tag model with articles array

---
+++ models/Tag.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const tagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  articles: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Article",
+  },
+});
+
+export const Tag = mongoose.model("Tag", tagSchema);
